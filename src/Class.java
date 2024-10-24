262570e932345d28adf22651ba317cdbd5b11499
Simon Kramer <simon.kramer@fau.de>
2024-10-24 10:48:29 +0200

    Tafelübung 1

--- src/Class.java
+++ src/Class.java
@@ -1,5 +1,29 @@
 public class Class {
+/* private static int switchMethod(int var) {
+        int ergebnis = 0;
+        switch (var) {
+            case 1:
+                ergebnis += 1;
+                break;
+            case 2:
+                ergebnis += 2;
+            case 3:
+                ergebnis += 4;
+                break;
+            case 4:
+                ergebnis += 8;
+            default:
+                ergebnis += 16;
+                break;
+        }
+        return ergebnis;
+    }*/
+
     public static void main(String[] args) {
         // Example class
+//        System.out.println(switchMethod(1));
+        int i = 42;
+        System.err.println("Hello World! " + i);
+        System.out.println(i);
     }
 }
---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        /*Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        System.out.println(i);
+        System.out.println("Hello World!");*/
+
+        System.out.println("a");
+        System.out.println('a');
+    }
+}
