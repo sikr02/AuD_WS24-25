4cf24e683cd2274b6c1c39f880e95df6852556cd
Simon Kramer <simon.kramer@fau.de>
2024-11-07 09:51:07 +0100

    Tafelübung 3

---
+++ src/T3Repetitorium.java
@@ -0,0 +1,34 @@
+public class T3Repetitorium {
+    // public static ... irgendwas mit int
+
+    // public static ... irgendwas mit double
+
+    // public static ... irgendwas mit void
+
+    // public static ... irgendwas mit int[]
+
+
+
+    public static String changeCase(String s) {
+        String res = "";
+        // Tipp: Schleife ueber s
+        // ...
+        return res;
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(quadrat(6));
+//        System.out.println(quadrat(4.0));
+
+//        int[] array1 = new int[]{1, 2, 3, 4, 5};
+//        inc1(array1);
+//        System.out.println("array1: " + java.util.Arrays.toString(array1));
+
+//        int[] array2 = inc2(array1);
+//        System.out.println("array1: " + java.util.Arrays.toString(array1));
+//        System.out.println("array2: " + java.util.Arrays.toString(array2));
+
+//        System.out.println(changeCase("aUd iST tOLL! :)"));
+//        System.out.println(changeCase("dAS WAR'S FUER HEUTE. bIS NAECHSTE wOCHE!"));
+    }
+}
---
+++ src/T3Teilbar.java
@@ -0,0 +1,29 @@
+public class T3Teilbar {
+    public static void alleTeiler(int x) {
+        // Implement ...
+        for (int i = 1; i <= x; i++) {
+            if (teilbar(x, i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean teilbar(int x, int y) {
+        // Implement ...
+        return x % y == 0;
+    }
+
+    public static void main(String[] args) {
+        int a1 = 100;
+        int a2 = 5;
+
+        boolean b = teilbar(a1, a2);
+        System.out.println(a1 + " ist durch " + a2 + " teilbar: " + b);
+
+        boolean c = teilbar(a2, a1);
+        System.out.println(a2 + " ist durch " + a1 + " teilbar: " + c);
+
+        alleTeiler(a1);
+        alleTeiler(a2);
+    }
+}
